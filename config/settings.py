f93b1a3e1a0706679e61b1f473b9051ec37afb0c
fpanos <fpanos@triarail.com>
2023-01-02 15:13:26 +0100

    Jan-01-08

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-f!mfv&gx^%ie1ge4cd$lwp-shfzb6*p4n9c24kus+nzt^g!_%z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trams-mercave.azurewebsites.net','*']
 
 
 # Application definition
