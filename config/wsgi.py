beed05562a952d9c7cc598a5d2fe9ee4a5e10eff
fpanos <fpanos@triarail.com>
2023-01-02 13:25:13 +0100

    Jan0204

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
 settings_module = 'config.production' if 'WEBSITE_HOSTNAME' in os.environ else 'config.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
