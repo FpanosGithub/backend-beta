0fbb3247f95e4ebdf8715b2bed8fe1ceff68ee37
fpanos <fpanos@triarail.com>
2023-03-01 22:04:37 +0100

    mar01 04

--- vehiculos/urls.py
+++ vehiculos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 # Ejes
-from vehiculos.views import SeleccionEjes,DetalleEje
+from vehiculos.views import SeleccionEjes,DetalleEje, Seleccion920, Seleccion760
 # Vehiculos
 from vehiculos.views import SeleccionVehiculos, DetalleVehiculo, SeleccionLocomotoras, SeleccionAuxiliares, SeleccionVagones
 
@@ -9,8 +9,8 @@ urlpatterns = [
     # EJES
     path('ejes', SeleccionEjes, name = 'ejes'),
     path('ejes/<int:id>/', DetalleEje, name = 'detalle_eje'),
-    path('ejes920', SeleccionEjes, name = 'ejes920'),
-    path('ejes760', SeleccionEjes, name = 'ejes760'),
+    path('ejes920', Seleccion920, name = 'ejes920'),
+    path('ejes760', Seleccion760, name = 'ejes760'),
     # VEHÍCULOS 
     path('', SeleccionVehiculos, name = 'vehiculos'),
     path('<int:id>/', DetalleVehiculo, name = 'detalle_vehiculo'),
