61d3fbc3dced7c58975930c5337afcb3c8f36d12
fpanos <fpanos@triarail.com>
2023-03-03 14:44:31 +0100

    mar0301

--- eventos/views.py
+++ eventos/views.py
@@ -4,9 +4,9 @@ from rest_framework.decorators import api_view, permission_classes
 
 # Importaciones de distintas apps
 # eventos
-from eventos.models import CirculacionVehiculo, EventoVehiculo, CirculacionEje, EventoEje
+from eventos.models import CirculacionVehiculo, EventoVehiculo, CirculacionEje, EventoEje, CambioEje
 from eventos.serializers import EventoVehiculoSerializer, CirculacionVehiculoSerializer, DatosCirculacionesVehiculoAmpliadas
-from eventos.serializers import CirculacionEjeSerializer, EventoEjeSerializer, DatosCirculacionesEjeAmpliadas
+from eventos.serializers import CirculacionEjeSerializer, EventoEjeSerializer, DatosCirculacionesEjeAmpliadas, CambioSerializer
 
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -59,7 +59,7 @@ def CirculacionesEje(request, id=1):
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     return Response(serializer.data)
 
-# api/eventos_circulacion_vehiculo
+# api/eventos_circulacion_eje
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def EventosCirculacionEje(request, id=1): 
@@ -69,12 +69,24 @@ def EventosCirculacionEje(request, id=1):
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     return Response(serializer.data)
 
-# eventos/circulaciones_vehiculo_ampliadas
+# eventos/circulaciones_eje_ampliadas
 @api_view(['GET'])
 @permission_classes([AllowAny])
 def CirculacionesEjeAmpliadas(request, id=1): 
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     serializer = DatosCirculacionesEjeAmpliadas(id_eje = id)
     print(serializer.data)
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    return Response(serializer.data)
+
+# eventos/cambios_eje
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def CambiosEje(request, id=1): 
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    # Vamos a componer una lista de cambios del eje solicitado
+    cambios = CambioEje.objects.filter(eje = id).order_by('-inicio')[:20]
+    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    serializer = CambioSerializer(cambios, many= True)
     #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     return Response(serializer.data)
